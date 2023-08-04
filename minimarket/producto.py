616cbffc8d8e9730f43c44d0cdc6d398715e503f
1998lu <138246090+1998lu@users.noreply.github.com>
2023-08-04 14:36:29 -0500

    producto.py

--- minimarket/producto.py
+++ minimarket/producto.py
@@ -10,6 +10,14 @@ class Producto:
     
     def actualizar_cantidad(self,cantidad_vendida):
         self.cantidad = self.cantidad - cantidad_vendida
+    
+    def get_nombre(self):
+        return self.nombre
+    def get_marca(self):
+        return self.marca
+    def get_info_completa(self):
+        return f"{self.nombre} {self.marca} {self.precio} { self.cantidad}"
+
 
 p= Producto("teclado", "HP", 50, 300)
 i= Producto("laptop", "HP", 2000, 300)
