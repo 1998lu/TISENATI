69aa87ab2c850f917feb3f05cb57d02b9c90cb0c
1998lu <138246090+1998lu@users.noreply.github.com>
2023-08-04 14:35:59 -0500

    operacion.py

--- minimarket/operacion.py
+++ minimarket/operacion.py
@@ -1,19 +1,46 @@
 from producto import Producto, input_int
-
+prueba = Producto("LAPTOP", "ASUS", 10000, 21)
 lista_producto =[]
+lista_producto.append(prueba)
 
-p = Producto("laptop", "asus", 2000, 5)
+#p = Producto("laptop", "asus", 2000, 5)
 #lista_producto.append(p)
 #print(lista_producto)
 #producto = lista_producto[0]
 #print(producto.info())
+def comparar_producto(np, mp, list_product):
+    existe = False
+    for p in list_product:
+        if np.upper() == p.get_nombre().upper() and mp.upper() == p.get_marca().upper():
+            print("este producto ya existe")
+            existe = True
+        else:
+            print("agregando producto")
+    return existe  #para saber si existe o no existe
 
-nombre_p = input("nombre del producto: ")
-marca_p = input("marca del producto: ")
-precio_p = input_int("precio del producto: ", "no ingresaste bien el precio")
-cantidad_p = input_int("cantidad del producto: ", "no ingresaste bien la cantidad")
+comparar_producto("laptop", "asus", lista_producto)
 
-t = Producto(nombre_p, marca_p, precio_p, cantidad_p)
-print(t.info())
+def crear_producto(list_product):
+    nombre_p = input("nombre del producto: ")
+    marca_p = input("marca del producto: ")
+    precio_p = input_int("precio del producto: ", "no ingresaste bien el precio")
+    cantidad_p = input_int("cantidad del producto: ", "no ingresaste bien la cantidad")
 
-lista_producto.append(t)
+    existencia = comparar_producto(nombre_p,marca_p, lista_producto)   
+    if not existencia:
+        nuevo_producto = Producto(nombre_p, marca_p, precio_p, cantidad_p)
+        list_product.append(nuevo_producto)
+
+#crear_producto(lista_producto)
+print(lista_producto)
+
+def listar_producto(list_product):
+    c = 0
+    for p in list_product:
+        c += 1
+        print(f"{c}. {p.get_info_nombre()}")
+lista_producto(listar_producto)
+
+#print(t.info())
+
+#lista_producto.append(t)
